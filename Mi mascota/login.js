48a07f39b6cb2ba96b88bffeb7f2cf735d2b91ea
Reynaldo Quintero <Reynaldo Quintero>
2017-02-23 00:19:17 -0400

    Login muy avanzado.

--- Mi mascota/login.js
+++ Mi mascota/login.js
@@ -2,15 +2,17 @@ function login(){
 	var email = document.getElementById("email").value;
 	var password = document.getElementById("password").value;
 	console.log("HALOS");
+	if(email!="" && password!=""){
+		$("#loginSpinner").show();
+		$("#loginBtn").hide();
+		firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(function(error) {
+	  		$("#loginError").show().text(error.message);
+	  		$("#loginSpinner").hide();
+			$("#loginBtn").show();
+	  	})
+	}
 
-	firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch(function(error) {
-	  	// Handle Errors here
-		console.log(error);
-	}).then(function(){
-		
-	})
-	var usuario = firebase.auth().currentUser.uid;
-	console.log(usuario);
-	window.location = "/index.html";
+	
+	
 	}
