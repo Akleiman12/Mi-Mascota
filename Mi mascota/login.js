971daaa1331d70b30b1a2734349fe2a25f806015
José Luis Raffalli <raffallijoseluis@gmail.com>
2017-02-23 22:40:25 -0430

    Sign In completo sin probar

--- Mi mascota/login.js
+++ Mi mascota/login.js
@@ -1,3 +1,23 @@
+firebase.auth().onAuthStateChanged(function(user){
+	if(user){
+		$(".pantallaLogin").hide();
+		$("#signin").hide();
+		$("#signout").show();
+	}
+	else{
+		$("#signin").show();
+		$("#signout").hide();
+	}
+})
+
+function muestraLogin(){
+	$(".pantallaLogin").show();
+}
+
+function ocultaLogin(){
+	$(".pantallaLogin").hide();
+}
+
 function login(){
 	var email = document.getElementById("email").value;
 	var password = document.getElementById("password").value;
@@ -10,9 +30,11 @@ function login(){
 	  		$("#loginError").show().text(error.message);
 	  		$("#loginSpinner").hide();
 			$("#loginBtn").show();
-	  	})
-	}
+	  	}).then(function(){
 
-	
-	
+	})
+	}
+	else{
+	alert("Debes llenar los campos necesarios");
+	}
 	}
